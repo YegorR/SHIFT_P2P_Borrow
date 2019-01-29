c15968718126714094757293311c176959917500
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 21:19:48 +0700

    Создан пакет data, создан файл percent.txt, который будет хранить информацию о ранжироваках процентов, создан класс PercentData, который обрабывает файл и выдаёт информацию в программном виде.

---
+++ src/main/java/ru/cft/starterkit/data/PercentData.java
@@ -0,0 +1,10 @@
+package ru.cft.starterkit.data;
+
+public class PercentData {
+    public class Row{
+        private double min;
+        private double max;
+        private double percent;
+
+    }
+}
