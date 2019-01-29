d0902f3685aba7cc445d8de70e7495a033d14805
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 16:14:54 +0700

    У админа добавлена команда "добавить заёмщика"

--- src/main/java/ru/cft/starterkit/api/AdminController.java
+++ src/main/java/ru/cft/starterkit/api/AdminController.java
@@ -2,9 +2,22 @@ package ru.cft.starterkit.api;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/add/borrower",
+            consumes = "application/x-www-form-urlencoded",
+            produces = "application/json"
+    )
+    public Boolean addOffer (
+            @RequestParam(name = "login") String login,
+            @RequestParam(name = "password") String password){
+        return true;
+    }
 }
