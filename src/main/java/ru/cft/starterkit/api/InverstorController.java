5fba57af8ee9740251cbc255c3917ed111ca6797
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 15:00:29 +0700

    Добавлено два простых метода контроллера

--- src/main/java/ru/cft/starterkit/api/InverstorController.java
+++ src/main/java/ru/cft/starterkit/api/InverstorController.java
@@ -1,8 +1,37 @@
 package ru.cft.starterkit.api;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.cft.starterkit.service.LogicService;
 
 @RestController
 public class InverstorController {
+
+    /*private final LogicService logicService;
+
+    @Autowired
+    public InverstorController (LogicService logicService){
+        this.logicService = logicService;
+    }*/
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/offer",
+            consumes = "application/x-www-form-urlencoded",
+            produces = "application/json"
+    )
+    public String addOffer (
+            @RequestParam(name = "sum") Double sum){
+        return "OK";
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/balance",
+            produces = "application/json"
+    )
+    public String getBalance(){
+        return "Balance";
+    }
 }
