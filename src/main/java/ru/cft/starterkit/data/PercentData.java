804c68c7d41ebe16149277c9657f53ce96a77b70
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 21:23:51 +0700

    Доделан внутренний класс Row, отвечает за одну строку "таблицы" процентов.

--- src/main/java/ru/cft/starterkit/data/PercentData.java
+++ src/main/java/ru/cft/starterkit/data/PercentData.java
@@ -2,9 +2,26 @@ package ru.cft.starterkit.data;
 
 public class PercentData {
     public class Row{
-        private double min;
-        private double max;
-        private double percent;
+        final private double min;
+        final private double max;
+        final private double percent;
 
+        Row(double min, double max, double percent){
+            this.min = min;
+            this.max = max;
+            this.percent = percent;
+        }
+
+        public double getMin() {
+            return min;
+        }
+
+        public double getMax() {
+            return max;
+        }
+
+        public double getPercent() {
+            return percent;
+        }
     }
 }
