f76ceedd61471d022b6192e84af86ca03a420d99
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 16:28:03 +0700

    Прописан интерфейс LogicService

--- src/main/java/ru/cft/starterkit/service/LogicService.java
+++ src/main/java/ru/cft/starterkit/service/LogicService.java
@@ -1,10 +1,21 @@
 package ru.cft.starterkit.service;
 
 import ru.cft.starterkit.entity.Borrower;
+import ru.cft.starterkit.entity.Investor;
+import ru.cft.starterkit.entity.Offer;
 import ru.cft.starterkit.entity.ServerOffer;
 
 public interface LogicService {
     ServerOffer createServerOffer(double sum, int term);
     void createDeal(ServerOffer serverOffer, Borrower borrower);
+
+    void payByBorrower(double sum);
+    double getBalanceBorrower(Borrower borrower);
+    int getTermBorrower(Borrower borrower);
+
+    Offer createOffer(double sum, double percent, Investor investor);
+
+    double getBalanceInvestor(Investor investor);
+    double getLoanInvestor(Investor investor);
 
 }
