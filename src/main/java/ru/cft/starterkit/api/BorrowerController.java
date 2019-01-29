c6c0067ec343cf06635d1292d74e6a9660a4c805
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 15:13:20 +0700

    Класс BorrowerController добавлен

---
+++ src/main/java/ru/cft/starterkit/api/BorrowerController.java
@@ -0,0 +1,12 @@
+package ru.cft.starterkit.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/borrower")
+public class BorrowerController {
+
+}
