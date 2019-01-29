5ab025910ac938975ea57e60bbdcfab927ad7f3b
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-01-29 16:41:00 +0700

    Добавлен класс Investor, взят класс User от Миши

--- src/main/java/ru/cft/starterkit/entity/Investor.java
+++ src/main/java/ru/cft/starterkit/entity/Investor.java
@@ -1,4 +1,28 @@
 package ru.cft.starterkit.entity;
 
 public class Investor extends User{
+    private Offer offer;
+    private double loan;
+
+    public Investor(String login, String password){
+        super(login, password);
+        loan = 0.0;
+        offer = null;
+    }
+
+    public double getLoan(){
+        return loan;
+    }
+
+    public void setLoan(double loan){
+        this.loan = loan;
+    }
+
+    public Offer getOffer(){
+        return offer;
+    }
+
+    public void setOffer(){
+        this.offer = offer;
+    }
 }
--- src/main/java/ru/cft/starterkit/entity/User.java
+++ src/main/java/ru/cft/starterkit/entity/User.java
@@ -1,4 +1,37 @@
 package ru.cft.starterkit.entity;
 
 public class User {
+    private String login;
+    private String password;
+    private double balance;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+        balance = 0;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
