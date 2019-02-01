a93d48770155ab67a13d04ef5235e3adb479076d
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-02-01 15:00:15 +0700

    Стандартизирован вывод у BorrowerController

--- src/main/java/ru/cft/starterkit/api/BorrowerController.java
+++ src/main/java/ru/cft/starterkit/api/BorrowerController.java
@@ -3,17 +3,14 @@ package ru.cft.starterkit.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import ru.cft.starterkit.entity.Borrower;
 import ru.cft.starterkit.entity.ServerOffer;
 import ru.cft.starterkit.exception.BorrowerNotFoundException;
 import ru.cft.starterkit.exception.ServerOfferNotFoundException;
-import ru.cft.starterkit.repository.implement.SampleEntityRepositoryImpl;
 import ru.cft.starterkit.service.AuthenticationService;
 import ru.cft.starterkit.service.LogicService;
 
@@ -41,21 +38,24 @@ public class BorrowerController {
             consumes = "application/x-www-form-urlencoded",
             produces = "application/json"
     )
-    public ServerOffer.ServerOfferForUser ask(
+    public Response ask(
             @RequestParam(name = "sum") Double sum,
             @RequestParam(name = "term") Integer term,
             @RequestParam(name = "id") UUID id){
+        Response response;
         try {
             log.info("Запрос: POST borrower/ask sum={}, term={}, id={}", sum, term, id);
             authenticationService.getBorrower(id);
         }
         catch(BorrowerNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            //throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            return new Response(false, null, 403, "Forbidden");
         }
 
         ServerOffer serverOffer = logicService.createServerOffer(sum, term);
-        if (serverOffer == null) return null;
-        return logicService.createServerOffer(sum, term).getServerOfferForUser();
+        if (serverOffer == null) response = new Response(true, null, 200, "OK");
+        else response = new Response(true, logicService.createServerOffer(sum, term).getServerOfferForUser(), 200, "OK");
+        return response;
     }
 
     @RequestMapping(
@@ -64,18 +64,19 @@ public class BorrowerController {
             consumes = "application/x-www-form-urlencoded",
             produces = "application/json"
     )
-    public boolean consent (
+    public Response consent (
             @RequestParam(name = "server_offer_id") UUID server_offer_id,
             @RequestParam(name = "id") UUID id) throws ServerOfferNotFoundException {
         try {
             log.info("Запрос: POST borrower/ok server_offer_id={},  id={}", server_offer_id, id);
             Borrower borrower = authenticationService.getBorrower(id);
-            logicService.createDeal(server_offer_id, borrower);
+            boolean answer = logicService.createDeal(server_offer_id, borrower);
+            return new Response(true, answer, 200, "OK");
         }
         catch(BorrowerNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            //throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            return new Response(false, null, 403, "Forbidden");
         }
-        return true;
     }
 
     @RequestMapping(
@@ -83,15 +84,16 @@ public class BorrowerController {
             path = "/balance",
             produces = "application/json"
     )
-    public double balance (
+    public Response balance (
             @RequestParam(name = "id") UUID id){
         try {
             log.info("Запрос: GET borrower/balance ,  id={}", id);
             Borrower borrower = authenticationService.getBorrower(id);
-            return borrower.getBalance();
+            return new Response(true, borrower.getBalance(), 200, "OK");
         }
         catch(BorrowerNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            //throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            return new Response(false, null, 403, "Forbidden");
         }
     }
 
@@ -101,17 +103,18 @@ public class BorrowerController {
             consumes = "application/x-www-form-urlencoded",
             produces = "application/json"
     )
-    public double pay (
+    public Response pay (
             @RequestParam(name = "sum") double sum,
             @RequestParam(name = "id") UUID id){
         try {
             log.info("Запрос: POST borrower/pay sum={},  id={}", sum, id);
             Borrower borrower = authenticationService.getBorrower(id);
             logicService.payByBorrower(sum, borrower);
-            return borrower.getBalance();
+            return new Response(true, borrower.getBalance(), 200, "OK");
         }
         catch(BorrowerNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            //throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+            return new Response(false, null, 403, "Forbidden");
         }
     }
 }
